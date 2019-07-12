c63d2ce85913a166a6cdec3995fa4c2ebb522917
seaman <seamanz380@gmail.com>
2019-07-12 20:50:05 +0800

    A recyclerview that respects the nested scrolling of its children (Translated from its kotlin version here https://medium.com/widgetlabs-engineering/scrollable-nestedscrollviews-inside-recyclerview-ca65050d828a ) Credits for the original code go to Marc Knaup.

--- app/src/main/java/com/example/customrecyclerview/NestedRecyclerView.java
+++ app/src/main/java/com/example/customrecyclerview/NestedRecyclerView.java
@@ -14,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
+ * A recyclerview that respects the nested scrolling of its children (Translated from its kotlin version here https://medium.com/widgetlabs-engineering/scrollable-nestedscrollviews-inside-recyclerview-ca65050d828a ) Credits for the original code go to Marc Knaup.
+ * https://gist.github.com/loukwn/237caeaf2088a55d4f939b8af7189d4c
  * Created by Seaman on 2019-07-12.
  * Banggood Ltd
  */
